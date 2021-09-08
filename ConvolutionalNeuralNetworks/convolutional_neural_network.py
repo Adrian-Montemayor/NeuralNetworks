d6ef5df277088315b1a34fdfac3dbddc84c6567f
Adrian-Montemayor <adrianmoncar@hotmail.com>
2021-09-07 22:57:34 -0500

    Part4 -  Making a single prediction

--- ConvolutionalNeuralNetworks/convolutional_neural_network.py
+++ ConvolutionalNeuralNetworks/convolutional_neural_network.py
@@ -63,3 +63,18 @@ cnn.add(tf.keras.layers.Dense(units=1, activation="sigmoid"))
 cnn.compile(optimizer = "adam", loss = "binary_crossentropy", metrics = ["accuracy"])
 #Training the CNN on the Training set and evaluating it on the test set
 cnn.fit(x = training_set, validation_data = test_set, epochs = 25)
+
+#Part4 - Making a single prediction
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img(path="./dataset/single_prediction/cat_or_dog_1.jpg", target_size=(64,64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = cnn.predict(test_image/255)
+training_set.class_indices
+if result[0][0] > 0.5:
+    prediction = "dog"
+else:
+    prediction = "cat"
+
+print(prediction)
