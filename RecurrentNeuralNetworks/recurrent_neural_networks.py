e86984c05f8fa084c2edc4d1fc233120f4511dbf
Adrian-Montemayor <adrianmoncar@hotmail.com>
2021-09-10 00:29:32 -0500

    RNN: Trash section, dont buy this course.

--- RecurrentNeuralNetworks/recurrent_neural_networks.py
+++ RecurrentNeuralNetworks/recurrent_neural_networks.py
@@ -38,7 +38,7 @@ print(X_train)
 print(y_train)
 
 #Reshaping
-X_train = np.reshape(X_train, (-1, 1, 1))
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 print(X_train)
 
 #Part2 - Building the RNN
@@ -48,6 +48,48 @@ from keras.layers import Dense, LSTM, Dropout
 #Initialising the RNN
 regressor = Sequential()
 #Adding the first LST layer and some Dropout regularisation
-#regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (np.array(X_train)[indices.astype(int)], 1)))
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+#Adding a second LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+#Adding a third LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+#Adding a fourth LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+#Adding the output layer
+regressor.add(Dense(units=1))
+
+#Compiling the RNN
+regressor.compile(optimizer="adam", loss="mean_squared_error")
+
+#Fitting the RNN to the Training Set
+regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
+
+#Getting the real stock price of 2017
+dataset_test = pd.read_csv("./Google_Stock_Price_Test.csv")
+real_stock_price = dataset_test.iloc[:, 1:2].values
+
+#Getting the predicted stock price of 2017
+dataset_total = pd.concat((dataset_train["Open"], dataset_test["Open"]), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1,1)
+inputs = sc.transform(inputs)
+X_test = []
+for i in range(60,80):
+    X_test.append(inputs[i-60:i,0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
 #Part3 - Making the predictions and visualising the results
+plt.plot(real_stock_price, color = "red", label="Real Google Stock Price")
+plt.plot(predicted_stock_price, color = "blue", label="Predicted google stock price")
+plt.title("Google Stock Price Prediction")
+plt.xlabel("Time")
+plt.ylabel("Google Stock Price")
+plt.show()
